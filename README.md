50d4e9d11db267b3b480df85b8645ad879fa1479
trry <54043933+tk-425@users.noreply.github.com>
2023-08-17 02:37:28 -0400

    update: v1.1.0 - Added a button to go back to the original style

---
+++ README.md
@@ -0,0 +1,4 @@
+# StackOverFlow Full Width Extension
+
+## About
+Enjoy a distraction-free view of questions and answers as sidebars are removed, and content stretches to fill the entire width. Say goodbye to clutter and hello to focused learning and collaboration. 
--- script.js
+++ script.js
@@ -1,28 +1,34 @@
 ﻿window.addEventListener('load', () => {
-  const elmColors = document.getElementsByName('change');
+  const full = document.getElementsByName('full');
+  const original = document.getElementsByName('original');
+  const fullWidth = true;
 
-  for (const element of elmColors) {
+  for (const element of full) {
     element.onclick = () => {
       chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         chrome.scripting.executeScript({
           target: { tabId: tabs[0].id },
           func: enable,
-          args: [''],
+          args: [fullWidth],
         });
       });
     };
   }
 
-  function enable() {
+  for (const element of original) {
+    element.onclick = () => {
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        chrome.scripting.executeScript({
+          target: { tabId: tabs[0].id },
+          func: enable,
+          args: [!fullWidth],
+        });
+      });
+    };
+  }
+
+  function enable(fullWidth) {
     console.log('Window loaded');
-
-    function displayNone(element) {
-      element.style.display = 'none';
-    }
-
-    function width100(element) {
-      element.style.width = '100%';
-    }
 
     // wait until passed selector is finished loading
     function waitForElement(selector) {
@@ -55,19 +61,24 @@
 
     waitForElement('.container')
       .then((container) => {
-        const leftSideBar = container.querySelector('#left-sidebar');
-        const content = container.querySelector('#content');
+        const leftSideBar = container.querySelector('#left-sidebar'); // 164px
+        const content = container.querySelector('#content'); // calc(100% - 164px)
         const innerContent = content.querySelector('.inner-content');
-        const mainBar = innerContent.querySelector('#mainbar');
-        const sideBar = innerContent.querySelector('#sidebar');
+        const mainBar = innerContent.querySelector('#mainbar'); // calc(100% - 300px - var(--su-static24))
+        const sideBar = innerContent.querySelector('#sidebar'); // 300px
 
         setTimeout(() => {
-          displayNone(leftSideBar);
-          width100(content);
-          width100(mainBar);
-          displayNone(sideBar);
-
-
+          if (fullWidth) {
+            leftSideBar.style.display = 'none';
+            content.style.width = '100%';
+            mainBar.style.width = '100%';
+            sideBar.style.display = 'hidden';
+          } else {
+            leftSideBar.style.display = 'flex';
+            content.style.width = 'calc(100% - 164px)';
+            mainBar.style.width = 'calc(100% - 300px - var(--su-static24))';
+            sideBar.style.display = 'visible';
+          }
         }, 1000);
       })
       .catch((error) => {
